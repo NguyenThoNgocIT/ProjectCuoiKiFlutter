2da95c315ad160ea5f65bf4012c2fbf090117cde
NguyenThoNgocIT <nguyenthongoc22072004@gmail.com>
2024-11-18 22:06:53 +0700

    định tuyến đường đi router cho cái app

---
+++ lib/router.dart
@@ -0,0 +1,21 @@
+import 'package:e_commerce_doancuoikingocit/features/screens/auth_screens.dart';
+import 'package:flutter/material.dart';
+
+Route<dynamic> generateRoute(RouteSettings routeSettings) {
+  switch (routeSettings.name) {
+    case AuthScreens.routeName:
+      return MaterialPageRoute(
+        settings: routeSettings,
+        builder: (_) => const AuthScreens(),
+      );
+    default:
+      return MaterialPageRoute(
+        settings: routeSettings,
+        builder: (_) => const Scaffold(
+          body: Center(
+            child: Text("Screen does not exist"),
+          ),
+        ),
+      );
+  }
+}
